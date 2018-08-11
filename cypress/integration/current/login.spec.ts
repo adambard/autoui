a18be36fed63e46afbe109c19cdc1122fe6292d7
Adam Bard <adam@adambard.com>
2018-08-10 21:25:36 -0700

    Sort out logins and do a test post

--- cypress/integration/current/login.spec.ts
+++ cypress/integration/current/login.spec.ts
@@ -1,98 +1,46 @@
 /// <reference types="Cypress" />
 
-declare namespace Cypress {
-    interface Chainable {
-        /**
-         * Yields "foo"
-         *
-         * @returns {typeof foo}
-         * @memberof Chainable
-         * @example
-         *    cy.foo().then(f => ...) // f is "foo"
-         */
-        loginBySingleSignOn(): Promise<any>;
-    }
-}
+import "../../support/commands";
 
 describe("Login", () => {
+    //let ringSessionId: string | null = null;
 
-    Cypress.Commands.add('loginBySingleSignOn', () => {
-        cy.request({
-            url: "https://dashboard.laterforreddit.com/auth/",
-            method: "GET",
-            followRedirect: false
-        }).its("headers").then(({ location }) => {
-            //const ringSessionId = resp.headers["set-cookie"][0].split(';')[0].slice(13);
-            const redditAuthLocation = location;
-
-            cy.request({
-                url: redditAuthLocation,
-                method: "GET"
-            }).then(resp => {
-                const $html = Cypress.$(resp.body);
-                const csrf = $html.find("input[name=csrf_token]").val();
-
-                // Perform login
-                return cy.request({
-                    url: "https://www.reddit.com/login",
-                    method: "POST",
-                    failOnStatusCode: false,
-                    form: true,
-                    followRedirect: false,
-                    body: {
-                        username: "BackgroundUnion",
-                        password: "bo0bo0bo0",
-                        dest: redditAuthLocation,
-                        csrf_token: csrf,
-                        otp: ""
-                    }
-                });
-            }).then(_ => {
-                const statePar = redditAuthLocation.match(/state=([^&]*)&/);
-                let state = "";
-                if (statePar != null) {
-                    state = statePar[1];
-                }
-
-                cy.request({
-                    url: redditAuthLocation,
-                    method: "GET",
-                }).then(getResp => {
-                    const $html = Cypress.$(getResp.body);
-                    console.log(getResp.body);
-                    const uh = $html.find("input[name=uh]").val();
-                    console.log("UH", uh);
-                    return cy.request({
-                        url: "https://www.reddit.com/api/v1/authorize",
-                        method: "POST",
-                        form: true,
-                        headers: {
-                            te: "Trailers"
-                        },
-                        body: {
-                            authorize: "Allow",
-                            client_id: "0cd0Jnk4Kmi0FQ", // TODO dynamicize
-                            duration: "permanent",
-                            redirect_uri: "https://laterforreddit.com/auth/callback",
-                            response_type: "code",
-                            scope: "flair+identity+modflair+modposts+submit",
-                            state,
-                            uh
-                        }
-                    });
-                }).then(authResp => {
-                    console.log(authResp);
-                });
-            });
-        });
+    before(() => {
+        cy.logInViaReddit("BackgroundUnion", "bo0bo0bo0");
+        Cypress.Cookies.preserveOnce("ring-session");
     });
 
     it("Can log in", () => {
-        cy.loginBySingleSignOn().then(r => {
-            console.log(r);
-            //cy.visit("https://dashboard.laterforreddit.com/auth/");
-        });
+        cy.visit("https://dashboard.laterforreddit.com/content/");
+        cy.get('.identity').should("contain", "adambard");
     });
+
+    it("Can create a post", () => {
+        cy.visit("https://dashboard.laterforreddit.com/content/create/");
+
+        cy.get('.identity').should("contain", "adambard");
+        cy.get('.title-field > input').type("Test Post Please Ignore");
+        cy.get('.link-field > input').type("https://laterforreddit.com");
+        cy.get('.submit-field > input').click();
+
+        cy.get('.subreddit-input > input').type("redditlater");
+        cy.get('.react-datepicker__input-container > input').type("{backspace}{backspace}{backspace}118");
+
+        cy.get('.loading-button').click();
+
+        cy.get('.subreddit').should("contain", "redditlater");
+        cy.get('.user').should("contain", "adambard");
+        cy.get('.date').should("contain", "2118");
+
+        cy.get('.delete-control > .control > .icon').click();
+        cy.get('.yes').click();
+
+        cy.get('.content-list > a').click();
+        cy.get(':nth-child(1) > .post-controls > .delete-control > .control > .icon').click();
+        cy.get('.yes').click();
+    });
+
+
     /*,
         it("Should work", () => {
             cy.visit("https://laterforreddit.com/");
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,3 +1,5 @@
+/// <reference types="Cypress" />
+
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
@@ -23,3 +25,82 @@
 //
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+declare namespace Cypress {
+    interface Chainable {
+        logInViaReddit(username: string, password: string): Cypress.Chainable<string>;
+    }
+}
+
+Cypress.Commands.add('logInViaReddit', (username: string, password: string) => {
+    cy.request({
+        url: "https://dashboard.laterforreddit.com/auth/",
+        method: "GET",
+        followRedirect: false
+    }).its("headers").then((headers) => {
+        const ringSessionId = headers["set-cookie"][0].split(';')[0].slice(13);
+        const redditAuthLocation = headers.location;
+
+        cy.request({
+            url: redditAuthLocation,
+            method: "GET"
+        }).then(resp => {
+            const $html = Cypress.$(resp.body);
+            const csrf = $html.find("input[name=csrf_token]").val();
+
+            // Perform login
+            return cy.request({
+                url: "https://www.reddit.com/login",
+                method: "POST",
+                failOnStatusCode: false,
+                form: true,
+                followRedirect: false,
+                body: {
+                    username,
+                    password,
+                    dest: redditAuthLocation,
+                    csrf_token: csrf,
+                    otp: ""
+                }
+            });
+        }).then(_ => {
+            const statePar = redditAuthLocation.match(/state=([^&]*)&/);
+            let state = "";
+            if (statePar != null) {
+                state = statePar[1];
+            }
+
+            cy.request({
+                url: redditAuthLocation,
+                method: "GET",
+            }).then(getResp => {
+                const $html = Cypress.$(getResp.body);
+                console.log(getResp.body);
+                const uh = $html.find("input[name=uh]").val();
+                console.log("UH", uh);
+                return cy.request({
+                    url: "https://www.reddit.com/api/v1/authorize.json",
+                    method: "POST",
+                    form: true,
+                    headers: {
+                        te: "Trailers"
+                    },
+                    body: {
+                        authorize: "Allow",
+                        client_id: "0cd0Jnk4Kmi0FQ", // TODO dynamicize
+                        duration: "permanent",
+                        redirect_uri: "https://laterforreddit.com/auth/callback",
+                        response_type: "code",
+                        //scope: "flair+identity+modflair+modposts+submit",
+                        scope: "identity",
+                        state,
+                        uh
+                    }
+                });
+            }).then(authResp => {
+                expect(authResp.status).to.equal(200);
+                return ringSessionId;
+            });
+        });
+    });
+});
