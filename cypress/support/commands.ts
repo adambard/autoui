2d25369745b17d2d3ab2150a364cefb455d83f73
Adam Bard <adam@adambard.com>
2018-08-22 21:14:19 -0700

    Refactor login not to fail if already logged in

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,106 +1,100 @@
 /// <reference types="Cypress" />
 
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This is will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
-
+// tslint:disable-next-line no-namespace
 declare namespace Cypress {
-    interface Chainable {
+    interface Chainable {  // tslint:disable-line interface-name
         logInViaReddit(username: string, password: string): Cypress.Chainable<string>;
     }
 }
 
-Cypress.Commands.add('logInViaReddit', (username: string, password: string) => {
-    cy.request({
+const getAuthRedirect = (): Cypress.Chainable<string> => {
+    return cy.request({
         url: "https://dashboard.laterforreddit.com/auth/",
         method: "GET",
-        followRedirect: false
-    }).its("headers").then((headers) => {
-        const ringSessionId = headers["set-cookie"][0].split(';')[0].slice(13);
-        const redditAuthLocation = headers.location;
-
-        cy.request({
-            url: redditAuthLocation,
-            method: "GET"
-        }).then(resp => {
-            const $html = Cypress.$(resp.body);
-            const csrf = $html.find("input[name=csrf_token]").val();
-
-            // Perform login
-            return cy.request({
-                url: "https://www.reddit.com/login",
-                method: "POST",
-                failOnStatusCode: false,
-                form: true,
-                followRedirect: false,
-                body: {
-                    username,
-                    password,
-                    dest: redditAuthLocation,
-                    csrf_token: csrf,
-                    otp: ""
-                }
-            });
-        }).then(_ => {
-            const statePar = redditAuthLocation.match(/state=([^&]*)&/);
-            let state = "";
-            if (statePar != null) {
-                state = statePar[1];
-            }
-
-            cy.request({
-                url: redditAuthLocation,
-                method: "GET",
-            }).then(getResp => {
-                const $html = Cypress.$(getResp.body);
-                console.log(getResp.body);
-                const uh = $html.find("input[name=uh]").val();
-                console.log("UH", uh);
-                return cy.request({
-                    url: "https://www.reddit.com/api/v1/authorize.json",
-                    method: "POST",
-                    form: true,
-                    headers: {
-                        te: "Trailers"
-                    },
-                    body: {
-                        authorize: "Allow",
-                        client_id: "0cd0Jnk4Kmi0FQ", // TODO dynamicize
-                        duration: "permanent",
-                        redirect_uri: "https://laterforreddit.com/auth/callback",
-                        response_type: "code",
-                        //scope: "flair+identity+modflair+modposts+submit",
-                        scope: "identity",
-                        state,
-                        uh
-                    }
-                });
-            }).then(authResp => {
-                expect(authResp.status).to.equal(200);
-                return ringSessionId;
-            });
-        });
+        followRedirect: false,
+    }).then(({ headers }) => {
+        return headers.location;
     });
+};
+
+const getRedditCsrfToken = (redditAuthLocation: string): Cypress.Chainable<string> => (
+    cy.request({
+        url: redditAuthLocation,
+        method: "GET",
+    }).then<string>((resp) => Cypress.$(resp.body).find("input[name=csrf_token]").val() as string)
+)
+
+interface ILoginOpts {
+    redditAuthLocation: string;
+    csrfToken: string;
+    username: string;
+    password: string;
+}
+const performLogin = ({ redditAuthLocation, csrfToken, username, password }: ILoginOpts) => (
+    cy.request({
+        url: "https://www.reddit.com/login",
+        method: "POST",
+        failOnStatusCode: false,
+        form: true,
+        followRedirect: false,
+        body: {
+            username,
+            password,
+            dest: redditAuthLocation,
+            csrf_token: csrfToken,
+            otp: "",
+        },
+    })
+)
+
+const getRedditModhash = (redditAuthLocation: string): Cypress.Chainable<string> => (
+    cy.request({
+        url: redditAuthLocation,
+        method: "GET",
+    }).then<string>(({ body }) => Cypress.$(body).find("input[name=uh]").val() as string)
+);
+
+const performRedditAuthorization = (redditAuthLocation: string, modHash: string): Cypress.Chainable<any> => {
+    const statePar = redditAuthLocation.match(/state=([^&]*)&/);
+    let state = "";
+    if (statePar != null) {
+        state = statePar[1];
+    }
+
+    return cy.request({
+        url: "https://www.reddit.com/api/v1/authorize.json",
+        method: "POST",
+        form: true,
+        headers: {
+            te: "Trailers",
+        },
+        body: {
+            authorize: "Allow",
+            client_id: "0cd0Jnk4Kmi0FQ", // TODO dynamicize
+            duration: "permanent",
+            redirect_uri: "https://laterforreddit.com/auth/callback",
+            response_type: "code",
+            // scope: "flair+identity+modflair+modposts+submit",
+            scope: "identity",
+            uh: modHash,
+            state,
+        },
+    });
+};
+
+Cypress.Commands.add("logInViaReddit", (username: string, password: string): Cypress.Chainable<any> => {
+    return getAuthRedirect().then(
+        (redditAuthLocation) => {
+            console.log(redditAuthLocation)
+            if (redditAuthLocation.search(/www.reddit.com/)) {
+                getRedditCsrfToken(redditAuthLocation).then((csrfToken) => (
+                    performLogin({ redditAuthLocation, csrfToken, username, password })
+                )).then(() => (
+                    getRedditModhash(redditAuthLocation)
+                )).then((modHash) => (
+                    performRedditAuthorization(redditAuthLocation, modHash)
+                ))
+            }
+        }
+    );
 });
