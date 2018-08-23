7a77bb5f810f7dc09bce25a5f0b153f6e24eae2d
Adam Bard <adam@adambard.com>
2018-08-22 19:57:59 -0700

    Half-working error screenshotting thing

--- cypress/integration/current/hello.spec.ts
+++ cypress/integration/current/hello.spec.ts
@@ -1,23 +1,23 @@
 /// <reference types="Cypress" />
 
-describe('Home Page Behaviour', () => {
-    beforeEach(function () {
-        cy.visit('https://laterforreddit.com/');
-    })
+describe("Home Page Behaviour", () => {
+    beforeEach(() => {
+        cy.visit("https://laterforreddit.com/");
+    });
 
-    it('should have a schedule post button', () => {
-        cy.get('.try-button')
-            .should('exist')
-            .and('have.attr', 'href')
-            .and('include', 'create')
+    it("should have a schedule post button", () => {
+        cy.get(".try-button")
+            .should("exist")
+            .and("have.attr", "href")
+            .and("include", "create")
             ;
     });
 
-    it('should have at least 1 create post link', () => {
-        cy.get('body')
+    it("should have at least 1 create post link", () => {
+        cy.get("body")
             .find('a[href*="/create"]')
             .then(
-                x => expect(x.length).above(1)
+                (x) => expect(x.length).above(1),
             );
     });
 });
